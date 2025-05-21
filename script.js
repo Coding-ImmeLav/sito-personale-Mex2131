151497487eda84b3a7bfe9c61a73cf0bfefddbc0
Mex <149148713+Mex2131@users.noreply.github.com>
2025-05-21 14:32:21 +0200

    sito

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let a = 0
-function comparimenu(){
-    if(a == 0) {
-        document.getElementById("menu").style.display = "initial"
-        a = 1
+let a = 0;
+
+function compariMenu(){
+    if(a==0) {
+        // se la a vale 0 succede quello che scrivo qui
+        document.getElementById("menu").style.display = "initial";
+        a = 1;  
+
     }
-    else if(a == 1) {
-        document.getElementById("menu").style.display = "none"
-        a = 0;
+
+    else if (a==1) {
+        document.getElementById("menu").style.display = "none";
+        a=0;
     }
 }
